c64e799a4876f2ef7369c806b6fac2aca05ff52b
MaganaOscar <oscmag13@gmail.com>
2022-06-22 10:59:43 -0700

    skeleton setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { LandingComponent } from './landing/landing.component';
+import { StorefrontComponent } from './storefront/storefront.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    LogoutComponent,
+    LandingComponent,
+    StorefrontComponent
   ],
   imports: [
     BrowserModule,
