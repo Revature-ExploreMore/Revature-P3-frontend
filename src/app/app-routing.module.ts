ca2134c069804a11a0f91397da166e5d223bc848
Goldendeep-kaur <kaurgoldendeep@gmail.com>
2022-06-23 18:53:01 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LoginComponent } from './user-info/login/login.component';
+import { RegisteruserComponent } from './user-info/registeruser/registeruser.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"login", component:LoginComponent},
+  {path:"registeruser", component:RegisteruserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
 import { LandingComponent } from './landing/landing.component';
 import { StorefrontComponent } from './storefront/storefront.component';
 import { CartComponent } from './cart/cart.component';
 import { UserComponent } from './user/user.component';
 import { OrdersComponent } from './orders/orders.component';
-import { DisplayComponent } from './display/display.component';
+//import { DisplayComponent } from './display/display.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './user-info/login/login.component';
+import { LogoutComponent } from './user-info/logout/logout.component';
+import { RegisteruserComponent } from './user-info/registeruser/registeruser.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    LogoutComponent,
+    HeaderComponent,   
     LandingComponent,
     StorefrontComponent,
     CartComponent,
     UserComponent,
-    OrdersComponent,
-    DisplayComponent,
-    CheckoutComponent
+    OrdersComponent,   
+    CheckoutComponent,
+         LoginComponent,
+         LogoutComponent,
+         RegisteruserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
