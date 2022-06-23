50d7f39e7f9882582fc60bbd789419beec66af34
MaganaOscar <oscmag13@gmail.com>
2022-06-23 14:03:06 -0700

    added course model details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StorefrontComponent } from './storefront/storefront.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "store", component: StorefrontComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
