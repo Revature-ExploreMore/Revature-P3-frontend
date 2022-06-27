efd26472a71dc3e0768a2db191b402522787eda4
arifes <arifesviryova@gmail.com>
2022-06-27 18:17:52 -0400

    methods added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../models/cart.model';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+//  currentCartItems: Cart [];
+//  cartMessage: string "";
+  
+ 
+  
+//  constructor(private cartService: CartService) { 
+ //   this.currentCartItems = [];
+  
 
   ngOnInit(): void {
+ //   this.loadData();
+  }
+}
+/*
+  loadData(){
+    this.currentCartItems;
   }
 
-}
+  getCartCourses(cartId: number){
+    this.cartService.getCartCourses(cartId).subscribe({
+      next: (response) => {
+        console.log(response);
+      //  this.cartMessage = '';
+        this.currentCartItems = response;
+      },
+ //     error: (error) => {
+ //       console.log(error.error.errorMessage);
+  //      this.cartMessage = error.error.errorMessage;
+  //    }
+    });
+  }
+
+  deleteItem(cartCourseId: number){
+    this.cartService.deleteItem(cartCourseId).subscribe((response)=>{
+      console.log(response);
+      this.loadData();
+    });
+  }
+  */
+
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cart } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  baseUrl: string = "http://localhost:7474/api/cartCourse"
+  constructor(private http: HttpClient) { }
+
+  getCartCourses(cartId: number): Observable<Cart[]>{
+    return this.http.get<Cart[]>(this.baseUrl+'/'+ cartId);
+  }
+
+  deleteItem(cartCourseId: number): Observable<boolean>{
+    return this.http.delete<boolean>(this.baseUrl+'/'+ cartCourseId);
+  }
 }
