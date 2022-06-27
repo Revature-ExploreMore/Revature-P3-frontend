3cc31e07272e733b72d42a5eb5ced5780c693cc5
berlyPaul360 <91963846+berlyPaul360@users.noreply.github.com>
2022-06-27 18:07:43 -0400

    added validators on form

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Billing } from '../models/billing.model';
 import { PaymentInfo } from '../models/payment.model';
 
 @Injectable({
@@ -9,11 +10,15 @@ import { PaymentInfo } from '../models/payment.model';
 export class CheckoutService {
 
   constructor(private http:HttpClient) { }
-  baseUrl:string = 'http://localhost/api/'
+  baseUrl:string = 'http://localhost:7474/api/'
 
   addPaymentInfo(paymentInfo:PaymentInfo):Observable<PaymentInfo>{
     return this.http.post<PaymentInfo>(this.baseUrl+'payment',paymentInfo);
 
+  }
+
+  addBillingInfo(billingInfo:Billing):Observable<Billing>{
+    return this.http.post<Billing>(this.baseUrl+'billing-address',billingInfo);
   }
 
 
