dcf4979c995b5f381d59beb985351d962f7def98
hangbin236 <hangbin236@revature.net>
2022-06-27 17:29:51 -0400

    started working get list of course function

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Course } from '../models/course.model';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  allCourse: Course [] = [] ;
+
+  newCategory: Category = {
+    categoryId: 0,
+    categoryName: ''
+  }
+
+  newCourse: Course = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    image_url: '',
+    category: this.newCategory
+  };
+
+  
+
+  constructor(
+              private router: Router) { 
+  
+  }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(){
+   // this.categoryHttpService.getAllCa
   }
 
 }
