383292cf5df5b67878b8d0d6f7b36d80bde00306
Landis Duffett <ldavduff@yahoo.com>
2022-07-01 14:44:21 -0700

    Update user.service.ts

--- src/app/user-info/user.service.ts
+++ src/app/user-info/user.service.ts
@@ -9,8 +9,8 @@ import { environment } from 'src/environments/environment.prod';
   providedIn: 'root'
 })
 export class UserService {
-  baseUrlLogin: string = "http://localhost:7474/user/user-info/";
-  //baseUrlLogin: string = environment.apiUrl+"/user/user-info";
+  //baseUrlLogin: string = "http://localhost:7474/user/user-info/";
+  baseUrlLogin: string = environment.apiUrl+"/user/user-info";
 
    constructor(private http: HttpClient) { }
 
