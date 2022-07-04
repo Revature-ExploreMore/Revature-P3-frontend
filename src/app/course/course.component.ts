609a0a37c2c3b93cd19fa85a6ca51d2cd4d524e0
jhebert85 <jhebert85@gmail.com>
2022-07-03 23:25:09 -0500

    course admin functions - view, add, modify

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,105 +5,116 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
 import { AuthService } from '../user-info/auth.service';
 
-
-
-
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit {
- 
   courses: Course[];
   categories: String[];
-  
+
   newCategory: Category = {
     id: 0,
-    categoryName: ""
-  }
+    categoryName: '',
+  };
 
   newCourse: Course = {
     id: 0,
-    name: "",
-    description: "",
+    name: '',
+    description: '',
     price: 0,
-    imageUrl: "",
-    category: this.newCategory
-  } 
+    imageUrl: '',
+    category: this.newCategory,
+  };
 
   updated: Course = {
     id: 0,
-    name: "",
-    description: "",
+    name: '',
+    description: '',
     price: 0,
-    imageUrl: "",
-    category: this.newCategory
-  }
+    imageUrl: '',
+    category: this.newCategory,
+  };
 
   constructor(
-    private courseService: CoursesService, 
+    private courseService: CoursesService,
     private authService: AuthService,
     private router: Router,
-    private activatedRoute: ActivatedRoute) 
-      {
-        this.courses = [];
-        this.categories = [];
-      }
-
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.courses = [];
+    this.categories = [];
+  }
 
   ngOnInit(): void {
     this.viewAllCourse();
     this.viewAllCategory();
+
+    let cidParam = this.activatedRoute.snapshot.paramMap.get('cid');
+    this.courseService.getACourse(cidParam).subscribe((response) => {
+    this.updated = response;
+    });
   }
 
   viewAllCourse() {
-    this.courseService.getAll().subscribe(response => {
-          return this.courses=response;
-        });
+    this.courseService.getAll().subscribe((response) => {
+      return (this.courses = response);
+    });
   }
 
-  viewAllCategory(){
-    this.courseService.getAll().subscribe(response => {
+  viewAllCategory() {
+    this.courseService.getAll().subscribe((response) => {
       console.log(response);
-      for(let course of response){
+      for (let course of response) {
         this.courses.push(course);
-        if(!this.categories.includes(course.category.categoryName)){
+        if (!this.categories.includes(course.category.categoryName)) {
           this.categories.push(course.category.categoryName);
         }
       }
-    })
+    });
   }
-  
-  addANewCourse(){
-    let course= this.courseService;
-    let addCourse:Course={
-    id: 0,
-    name: this.newCourse.name,
-    description: this.newCourse.description,
-    price: 0,
-    imageUrl: this.newCourse.imageUrl,
-    category: this.newCategory
-    };
 
-    this.newCourse={
-    id: 0,
-    name: "",
-    description: "",
-    price: 0,
-    imageUrl: "",
-    category: this.newCategory
+  addCourse() {
+    switch(this.newCourse.category.categoryName){
+      case "science":{
+        this.newCourse.category.id = 1;
+        break;
+      }
+      case "math":{
+        this.newCourse.category.id = 2;
+        break;
+      }
+      case "programming":{
+        this.newCourse.category.id = 3;
+        break;
+      }
+      case "writing":{
+        this.newCourse.category.id = 4;
+        break;
+      }
+      case "photography":{
+        this.newCourse.category.id = 5;
+        break;
+      }
+      case "digital art":{
+        this.newCourse.category.id = 6;
+        break;
+      }
     }
-  
-    this.courseService.addNewCourse(addCourse).subscribe((response)=>{
+
+    this.courseService.addNewCourse(this.newCourse).subscribe((response) => {
       console.log(response);
-    })
+    });
   }
-  
-  updateCourse(){
-    this.courseService.updatedCourse(this.updated).subscribe((response)=>{
+
+  setCurrentCourse(course: Course) {
+    this.updated = course;
+  }
+
+  updateCourse() {
+    this.courseService.updatedCourse(this.updated).subscribe((response) => {
       this.router.navigate(['course']);
     });
   }
 }
-
