9be558c757764387306151752dbb8d6ea06d9257
Landis Duffett <ldavduff@yahoo.com>
2022-07-05 13:41:26 -0700

    Update cart.service.ts

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,8 +11,8 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class CartService {
-  baseURL: string = "http://localhost:7474/cart/";
-  //baseURL: string = environment.apiUrl+"/cart/";
+  //baseURL: string = "http://localhost:7474/cart/";
+  baseURL: string = environment.apiUrl+"/cart/";
 
   constructor(private http: HttpClient) { }
 
